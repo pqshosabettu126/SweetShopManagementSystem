4ccc0dc31c849dae494ff205a891c433c26e29be
pqshosabettu126 <pqshosabettu@gmail.com>
2025-11-01 23:33:58 +0530

    feat(frontend): setup React + Vite + TypeScript + Tailwind base structure

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <h1 className="text-3xl font-bold text-blue-600">
+        Sweet Shop Management System
+      </h1>
+    </div>
+  )
+}
+
+export default App
+
+
+
